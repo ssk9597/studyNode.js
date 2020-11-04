6fd50ed960eaea011538e1611cb184e19203f3c7
Ryo Sasaki <wor.ryosasaki@gmail.com>
2020-11-04 21:06:56 +0900

    mongodb

--- task2/app.js
+++ task2/app.js
@@ -1,12 +1,16 @@
 //modules
 const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //controllers
+// for routing
 const indexController = require('./controllers/index');
 const loginController = require('./controllers/login');
 const registerController = require('./controllers/register');
-
-//models
+//for registerForm
+const storeRegisterController = require('./controllers/storeRegister');
 
 //Object
 const app = express();
@@ -16,13 +20,24 @@ app.set('view engine', 'ejs');
 
 //publicFolderUse
 app.use('/public', express.static(__dirname + '/public'));
+//moduleUse
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//DB接続（データベース名：node_task2）
+mongoose.connect('mongodb://localhost/node_task2', {
+    useNewUrlParser: true,
+});
 
 //routing
 app.get('/', indexController);
 app.get('/login', loginController);
-app.get('/register', registerController);
+app.get('/register', validationMiddleware, registerController);
+
+//post
+app.post('/users/register', storeRegisterController);
 
 //server
-app.listen(3000, () => {
-    console.log(`http://localhost:3000`);
+app.listen(5000, () => {
+    console.log(`http://localhost:5000`);
 });
