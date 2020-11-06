5a3e3b1a812650d6f6e863cc80ef5d2b6f965d2e
Ryo Sasaki <wor.ryosasaki@gmail.com>
2020-11-06 23:49:15 +0900

    mongoDBのcollection名を変更

--- task2/index.js
+++ task2/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //DB
-mongoose.connect('mongodb://localhost/node_nonDesign_task2', {
+mongoose.connect('mongodb://localhost/node_task2', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
